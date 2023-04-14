7cf2f54181fa254eae48154ee7e44d64df9dab7c
Guille <guillermoanez92@gmail.com>
2023-04-14 17:17:20 -0600

    latets on syllabus

--- lib/framework/prismic-php/templates/home.php
+++ lib/framework/prismic-php/templates/home.php
@@ -60,17 +60,38 @@ echo "</pre>";w
 	<p>2 c</p>
 	<div class="container">
 		<div class="container-inner">
-		<?php foreach($data->div_repeater as $block){ ?>
-			<div class="empty-div">
+			<?php foreach($data->div_repeater as $block){ ?>
+				<div class="empty-div">
 
-			</div>
-		<?php } ?>
+				</div>
+			<?php } ?>
 		</div>
 	</div>
 </div>
 <div class="functions">
 	<button class="function-btn">Hello</button>
 	<button class="function-btn">Bye</button>
+</div>
+<div class="new-prep-tabs c-24">
+	<div class="left">
+		<button class="active" data-dog="all">All</button>
+		<button data-dog="real">Real Dogs</button>
+		<button data-dog="comic">Comic Dogs</button>
+	</div>
+	<div class="right">
+		<div class="tile" data-dog="comic">
+			<img class="preload bg" src="https://images.prismic.io/syllabus/1082628c-df4b-4c1d-a810-8639941789f0_CatDog.jpeg?auto=compress,format">
+		</div>
+		<div class="tile" data-dog="real">
+			<img class="preload bg" src="https://images.prismic.io/syllabus/a2740883-4f08-4ac9-8cf1-95f650efa5d7_AustralianCattleDog-FeaturedImage-1024x615.webp?auto=compress,format">
+		</div>
+		<div class="tile" data-dog="real">
+			<img class="preload bg" src="https://images.prismic.io/syllabus/4cb2a621-48aa-4cd2-8daa-d71b5365e51e_husky-wolf-1.webp?auto=compress,format">
+		</div>
+		<div class="tile" data-dog="real">
+			<img class="preload bg" src="https://images.prismic.io/syllabus/02638aa3-a025-4528-9a80-e0ce8c7b413a_Pug_FeaturedImage.avif?auto=compress,format">
+		</div>
+	</div>
 </div>
 <?php
 /* ----- Home JSON+LD Schema ----- */
